7c032d12b5e015aa47d8998f69b68b89f34d0fd2
José Vitoriano <vitorianoamorim7@gmail.com>
2026-03-09 22:27:37 -0300

    trabalhando com vetores e matrizes

---
+++ HTML_CSS_JAVASCRIPT_PHP/ex15/estrutura.js
@@ -0,0 +1,13 @@
+// Estruturas de Repetição
+
+let somaTotal = 0;
+const qtdeInteracoes = 0;
+
+for(let i = qtdeInteracoes; i >= 1; i--){
+
+    somaTotal += i; //equivalente a utilizar somaTotal = somaTotal + 1
+
+}
+
+console.log('soma total:', somaTotal);
+
---
+++ HTML_CSS_JAVASCRIPT_PHP/ex18/manipulando_vetores.js
@@ -0,0 +1,17 @@
+
+var frutas = ['laranja', 'Uva', 'Limão', 'Jambo', 'Maça'];
+
+delete frutas [0];
+
+console.log(frutas);
+
+frutas.pop(4);
+console.log(frutas);
+
+frutas.shift(3);
+console.log(frutas);
+
+alert (frutas.length);
+frutas.length = 2;
+console.log(frutas.length);
+
