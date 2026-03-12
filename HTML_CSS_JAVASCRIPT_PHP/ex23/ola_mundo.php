c31bcf99a40e641d8fd6d5c3b2ed895b4e9dc26b
José Vitoriano <vitorianoamorim7@gmail.com>
2026-03-11 21:56:56 -0300

    Introdução com PHP

---
+++ HTML_CSS_JAVASCRIPT_PHP/ex23/ola_mundo.php
@@ -0,0 +1,19 @@
+
+
+<?php
+
+
+echo "Olá, Mundo\n";
+$var1 = 2 + 2;
+
+echo "O resultado da soma é igua a: ".$var1;
+
+
+
+$_nomeCurso = "Programação de páginas Dinâmicas com PHP";
+$ano_criacao = 1994;
+$flagLinguagemScript = true;
+
+
+
+
