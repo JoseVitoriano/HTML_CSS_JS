1b343fb2fbc99661b37512c8cb43b5f33854040f
José Vitoriano <vitorianoamorim7@gmail.com>
2026-03-15 13:39:48 -0300

    modficações

---
+++ HTML_CSS_JAVASCRIPT_PHP/ex24/banco_dados.php
@@ -0,0 +1,12 @@
+<!--Analise da interface entre Html e PHP-->
+
+<html>
+    <body>
+        
+    Seja bem vindo <?php echo $_POST["name"]; ?><br>
+    Acesso permitido: <?php echo $_POST["senha"];?>
+
+    
+
+    </body>
+</html>
