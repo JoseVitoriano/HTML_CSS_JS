0f488cf87b5067bc73651d9301bae233e64727f5
José Vitoriano <vitorianoamorim7@gmail.com>
2026-03-17 20:33:45 -0300

    trabalhando com condicoes

---
+++ HTML_CSS_JAVASCRIPT_PHP/ex26/condicao_while.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$i = 2;
+
+while($i <= 20){
+    echo $i;
+    $i += 2;
+    echo "\n";
+}
+
+
+/* Sintaxe Alternativa*/
+
+$i = 2;
+
+while ($i <= 20):
+    echo $i;
+    $i += 2;
+    echo"\n";
+endwhile;
+
