899b1e60a3c6efa20f7ddd928c92afff531351c3
José Vitoriano <vitorianoamorim7@gmail.com>
2026-03-17 20:57:36 -0300

    condicao php

--- HTML_CSS_JAVASCRIPT_PHP/ex26/condicao_do_while.php
+++ HTML_CSS_JAVASCRIPT_PHP/ex26/condicao_do_while.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
 <?php
 
 $i = 2;
@@ -6,3 +16,8 @@ do {
     $i +=2;
     echo "\n";
 } while ($i <= 20);
+
+?>
+    
+</body>
+</html>
