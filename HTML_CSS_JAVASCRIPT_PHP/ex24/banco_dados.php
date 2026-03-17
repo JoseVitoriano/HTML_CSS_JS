81cfd612e26dfcb970d4512f347711173b2ce63e
José Vitoriano <vitorianoamorim7@gmail.com>
2026-03-16 21:55:38 -0300

    trabalhando as estruturas condicionais

--- HTML_CSS_JAVASCRIPT_PHP/ex24/banco_dados.php
+++ HTML_CSS_JAVASCRIPT_PHP/ex24/banco_dados.php
@@ -1,12 +1,15 @@
 <!--Analise da interface entre Html e PHP-->
-
-<html>
-    <body>
-        
-    Seja bem vindo <?php echo $_POST["name"]; ?><br>
-    Acesso permitido: <?php echo $_POST["senha"];?>
-
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senha Bem Vindo</title>
+</head>
+<body>
+    seja bem vindo <?php echo $_POST["name"]; ?><br>
+    acesso permitido: <?php echo $_POST["senha"]; ?>
+    <br>
     
-
-    </body>
+</body>
 </html>
---
+++ HTML_CSS_JAVASCRIPT_PHP/ex25/switch.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+$var1 = 10;
+
+switch($var1){
+    case 10:
+        echo "$var1 é igual à 10.";
+        break;
+    case 20:
+        echo "$var1 é igual à 20";
+        break;
+    case 30:
+        echo "$var1 é igual à 30";
+        break;
+    default:
+        echo "$var1 não é igual à 10, 20 e nem à 30.";
+        break;
+    
+}
